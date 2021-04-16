d0eb0f2392abd8ca1e8dc7ad11b30f55b30b62a4
ricky <ricky.chaggar@gmail.com>
2021-04-16 11:47:56 +0100

    set up initial ejs files

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 
 const express = require("express");
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-
   var today = new Date();
   var currentDate = today.getDay();
 
