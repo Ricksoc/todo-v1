0bf7802b3d012f4cf7579f89f4c87981fd1b71d2
ricky <ricky.chaggar@gmail.com>
2021-04-16 13:02:30 +0100

    modified code to use <% %> scriptlet tags to render h1 style differently depending on if it is a weekday or weekend

--- app.js
+++ app.js
@@ -19,14 +19,14 @@ const days = [
 app.get("/", function (req, res) {
   var today = new Date();
   var currentDate = today.getDay();
+  var day = days[currentDate];
 
   if (currentDate > 6 || currentDate < 0) {
     console.log("Error: currentDay is equal to: " + currentDate);
   } else {
-    var day = days[currentDate];
+    res.render("list", { kindOfDay: day });
   }
 
-  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, () => {
