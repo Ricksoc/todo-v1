136089fdf1982d8769e00d818022d17563f86586
ricky <ricky.chaggar@gmail.com>
2021-04-16 12:38:08 +0100

    Set up list.ejs + app.js to render what day it is a a h1. Used array of day names with Date().getDay(). Added if statement to catch if current day is out of range

--- app.js
+++ app.js
@@ -6,15 +6,27 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Satuarday",
+];
+
 app.get("/", function (req, res) {
   var today = new Date();
   var currentDate = today.getDay();
 
-  if (currentDate === 6 || currentDate === 0) {
-    res.sendFile(__dirname + "/index.html");
+  if (currentDate > 6 || currentDate < 0) {
+    console.log("Error: currentDay is equal to: " + currentDate);
   } else {
-    res.sendFile(__dirname + "/index.html");
+    var day = days[currentDate];
   }
+
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, () => {
